f8ea609194d24367bad6e662a9da88cadc0dcae8
Keanu Reaño <reanokeanu@gmail.com>
2025-07-03 20:39:09 +0800

    install mdx

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
+  pageExtensions: ["tsx", "ts", "jsx", "js", "mdx", "md"],
   output: "export",
   basePath: process.env.PAGES_BASE_PATH || "",
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  extension: /\(md|mdx\)$/,
+});
+
+export default withMDX(nextConfig);
