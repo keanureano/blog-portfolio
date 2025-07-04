de8b4b9ad829c2bd855aaa10be34b2de2a571a0d
Keanu Reaño <reanokeanu@gmail.com>
2025-07-04 08:33:22 +0800

    add google docs resume

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <div>Keanu Reaño</div>;
+  return (
+    <main>
+      <section className="relative flex h-screen w-full items-center justify-center overflow-hidden bg-white">
+        <iframe
+          className="h-full w-[1200px] border-none"
+          src="https://docs.google.com/document/d/1IM-b3mo8iT4jD5upw1qPn9-f33RWMkfQl_C4jxGj2rU/preview"
+          allowFullScreen
+        />
+      </section>
+    </main>
+  );
 }
