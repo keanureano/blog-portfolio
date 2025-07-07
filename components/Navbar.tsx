24f0d9f3005f1e37873d61e219e2e0b02b2d2708
Keanu Reaño <reanokeanu@gmail.com>
2025-07-07 09:29:00 +0800

    remove buggy hamburger menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,13 +12,13 @@ export default function Navbar() {
       {/* Logo */}
       <Link
         href="/"
-        className={`${dotoMono.className} hover:drop-shadow-glow text-3xl font-bold text-neutral-50 transition`}
+        className={`${dotoMono.className} hover:drop-shadow-glow text-xl font-bold text-neutral-50 transition lg:text-3xl`}
       >
         {frontmatter.logo}
       </Link>
 
       {/* Desktop menu */}
-      <ul className="hidden space-x-4 lg:flex">
+      <ul className="flex space-x-4">
         {frontmatter.links.map((link: any) => (
           <li key={link.name}>
             <Link
@@ -30,33 +30,6 @@ export default function Navbar() {
           </li>
         ))}
       </ul>
-
-      {/* Hamburger toggle */}
-      <div className="group relative lg:hidden">
-        <input type="checkbox" id="menu-toggle" className="peer hidden" />
-        <label
-          htmlFor="menu-toggle"
-          className="flex cursor-pointer flex-col space-y-1 transition hover:brightness-75"
-        >
-          <span className="h-0.5 w-6 bg-neutral-300" />
-          <span className="h-0.5 w-6 bg-neutral-300" />
-          <span className="h-0.5 w-6 bg-neutral-300" />
-        </label>
-
-        {/* Mobile menu */}
-        <ul className="absolute right-0 z-50 mt-2 hidden w-48 flex-col space-y-2 bg-black p-4 peer-checked:flex">
-          {frontmatter.links.map((link: any) => (
-            <li key={link.name}>
-              <Link
-                href={link.href}
-                className="hover:drop-shadow-glow block transition hover:text-neutral-50"
-              >
-                {link.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
     </nav>
   );
 }
