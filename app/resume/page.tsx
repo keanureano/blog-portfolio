b4efcf1058435395cf15b6cd15c76b289a508be8
Keanu Reaño <reanokeanu@gmail.com>
2025-07-14 09:05:30 +0800

    update bn entry

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -5,8 +5,11 @@ export default async function ResumePage() {
   const { frontmatter } = await getMdx("cms/home.mdx");
   return (
     <>
+      <h2>
+        <em>Resume</em>
+      </h2>
       <Link href={`${frontmatter.resume}/export?format=pdf&tab=t.0`}>
-        <em className="ml-5">Download</em>
+        <strong className="ml-5">Download</strong>
       </Link>
       <iframe
         src={`${frontmatter.resume}/preview?tab=t.0`}
